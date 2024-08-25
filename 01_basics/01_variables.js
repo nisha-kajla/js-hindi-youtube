c95d06777c118753f21c6ef3a30c333fceab2ffb
nisha-kajla <101744583+nisha-kajla@users.noreply.github.com>
2024-08-25 06:59:01 +0000

    msg

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=144567
+let accountEmail="agam@goggle.com"
+var accountPassword="12345"
+accountCity="Punjab"
+let accountState;
+
+//accountId=2// not allowed
+
+accountEmail="nav@k.com"
+accountPassword="2121213"
+accountCity="jaipur"
+
+console.log(accountId)
+
+/*
+Prefer not to use var
+because of issuse in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
