37e1a075a73acce8d1f9a1493f29931163e76135
nisha-kajla <101744583+nisha-kajla@users.noreply.github.com>
2024-08-28 13:22:06 +0000

    DataTypeConversion

---
+++ 01_basics/03_DatatypeConversion.js
@@ -0,0 +1,28 @@
+let score=undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0;
+
+let isloggedIn="Nav"
+
+let booleanIsloggedIn=Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+
+//1=>true; 0=>false;
+//"=>false";
+//"Nav"=>true;
+
+letsomeNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+
+
