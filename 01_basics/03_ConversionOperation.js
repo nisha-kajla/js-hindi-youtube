f0df8652bdb6823d3a14cd659181f721cbf8bc4b
nisha-kajla <101744583+nisha-kajla@users.noreply.github.com>
2024-09-03 09:31:25 +0000

    conversion

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,49 @@
+let score=undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0;
+
+let isloggedIn="Nav"
+
+let booleanIsloggedIn=Boolean(isloggedIn)
+//console.log(booleanIsloggedIn);
+
+//1=>true; 0=>false;
+//"=>false";
+//"Nav"=>true;
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+/***************Operatiions*********************/
+
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+
+let str1="hello"
+let str2= " Agam"
+
+let str3= str1 + str2
+//console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" +2 +2);
+console.log(1 + 2 +"2") ;                                                                             
+
+
+
+
+
